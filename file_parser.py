1bd9b2ec4e0f5dd3bfaa3b043df36fa8ebe5bb55
jbson_x240 <jb@jbson.net>
2021-10-10 23:57:18 +0200

    update model search

--- file_parser.py
+++ file_parser.py
@@ -1,33 +1,16 @@
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from genericpath import isdir
-import struct
-from struct import unpack
 from os import makedirs, listdir
-from os.path import join, exists, isfile
+from os.path import join, exists, isfile, isdir
 
-from h5py._hl import dataset
-import constants as cnst
-from skimage.io import imsave
-import bs_lib.bs_string as bs
-import bs_lib.bs_eda as beda
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from skimage.io import imsave
 from sklearn.ensemble import IsolationForest
 import h5py
+
+import bs_lib.bs_string as bs
+import bs_lib.bs_eda as beda
+
 
 def as_filename(id, with_category, extension='jpeg'):
     return '{:05d}_{}.{}'.format(id, with_category, extension)
@@ -38,14 +21,16 @@ def get_category_name(from_filename):
     category = bs.extract_string(from_string=name, at_position=3)
     return category
 
-def get_images(from_image_path, nb_to_get=None,image_shape=None):
+
+def get_images(from_image_path, nb_to_get=None, image_shape=None):
     images = np.load(from_image_path)
     if nb_to_get:
         idx = np.random.choice(images.shape[0], nb_to_get, replace=False)
         images = images[idx]
-    if isinstance(image_shape,tuple):
+    if isinstance(image_shape, tuple):
         images = images.reshape(image_shape)
     return images
+
 
 def get_rows_from(npy_file_path, nb_random_rows=None):
     array = np.load(npy_file_path)
@@ -53,6 +38,7 @@ def get_rows_from(npy_file_path, nb_random_rows=None):
         idx = np.random.choice(array.shape[0], nb_random_rows, replace=False)
         array = array[idx]
     return array
+
 
 def get_random_images(from_image_path, nb_to_get, image_shape=(-1, 28, 28)):
     """Get nb random choosed image from a .npy files
@@ -71,7 +57,8 @@ def get_random_images(from_image_path, nb_to_get, image_shape=(-1, 28, 28)):
     images = images.reshape(image_shape)
     return images
 
-def save_as_binary(array,as_filename,  into_directory, verbose=False):
+
+def save_as_binary(array, as_filename,  into_directory, verbose=False):
     if not exists(into_directory):
         if verbose:
             print(f"Make directory: {into_directory}")
@@ -80,9 +67,10 @@ def save_as_binary(array,as_filename,  into_directory, verbose=False):
     if verbose:
         print(f"Saving array into {into_directory}")
 
-    np.save(join(into_directory,as_filename),arr=array)
+    np.save(join(into_directory, as_filename), arr=array)
     if verbose:
-        print(f"Array was saved as {as_filename} in directory: {into_directory}")
+        print(
+            f"Array was saved as {as_filename} in directory: {into_directory}")
 
 
 def save_as_jpeg(images, as_category, into_directory, verbose=False):
@@ -115,6 +103,7 @@ def save_as_jpeg(images, as_category, into_directory, verbose=False):
     if verbose:
         print(f"{count} images were saved in {as_category} directory")
 
+
 def merge_files(files):
     """Stack all files received. Files are npy files.
 
@@ -123,71 +112,76 @@ def merge_files(files):
 
     Returns:
         ndarray: merged arrays
-    """    
+    """
     merged_array = []
     for filename, file in files.items():
         array = np.load(file)
-        if len(merged_array)<1:
-            merged_array=array
+        if len(merged_array) < 1:
+            merged_array = array
         else:
             merged_array = np.vstack(array)
     return merged_array
 
-def create_dataset(dataset_name,from_directory_path, labels_dict, dest_directory_path=None, match_terms=[], exclude_terms=[], verbose=False):
+
+def create_dataset(dataset_name, from_directory_path, labels_dict, dest_directory_path=None, match_terms=[], exclude_terms=[], verbose=False):
     # takes all files needed from_directory_path
-    files = beda.get_list_dir(from_directory_path, with_extension='.npy',match_terms=match_terms,exclude_terms=exclude_terms)
+    files = beda.get_list_dir(from_directory_path, with_extension='.npy',
+                              match_terms=match_terms, exclude_terms=exclude_terms)
     # create destination path
     if dest_directory_path:
-        dataset_file_path = join(dest_directory_path,dataset_name)
+        dataset_file_path = join(dest_directory_path, dataset_name)
     else:
-        dataset_file_path = join(from_directory_path,dataset_name)
+        dataset_file_path = join(from_directory_path, dataset_name)
     # write to h5 file
-    with h5py.File(name=f"{dataset_file_path}.h5",mode="a") as f:
+    with h5py.File(name=f"{dataset_file_path}.h5", mode="a") as f:
         # create empty dataset into h5
-        f.create_dataset(name='image',shape=(0,784), maxshape=(None,784))
-        f.create_dataset(name='category',shape=(0,),maxshape=(None,),dtype=int)
+        f.create_dataset(name='image', shape=(0, 784), maxshape=(None, 784))
+        f.create_dataset(name='category', shape=(0,),
+                         maxshape=(None,), dtype=int)
 
-        for key,file in files.items():
-            category=key
+        for key, file in files.items():
+            category = key
             for w in match_terms:
-                category = category.replace(w,'')
-            category = category.replace('_','')
-            #load file content
-            file_path = join(from_directory_path,file)
+                category = category.replace(w, '')
+            category = category.replace('_', '')
+            # load file content
+            file_path = join(from_directory_path, file)
             images = np.load(file_path)
             # update dataset with data from files
             nb_images, _ = images.shape
-            f["image"].resize((f["image"].shape[0] + nb_images), axis = 0)
+            f["image"].resize((f["image"].shape[0] + nb_images), axis=0)
             f["image"][-nb_images:] = images
-            f["category"].resize((f["category"].shape[0] + nb_images), axis = 0)
-            f["category"][-nb_images:] = np.full(nb_images,labels_dict[category])
+            f["category"].resize((f["category"].shape[0] + nb_images), axis=0)
+            f["category"][-nb_images:] = np.full(
+                nb_images, labels_dict[category])
     if verbose:
         print(f"File created @ {dataset_file_path}")
     return f"{dataset_file_path}.h5"
 
-def load_dataset(dataset_filename,from_directory_path, verbose=False):
+
+def load_dataset(dataset_filename, from_directory_path, verbose=False):
     if verbose:
         print(f"loading {dataset_filename} @ {from_directory_path}")
-    file_path = join(from_directory_path,dataset_filename)
+    file_path = join(from_directory_path, dataset_filename)
     X_image = None
     y_category = None
     if isfile(file_path):
-        with h5py.File(file_path,'r') as file:
+        with h5py.File(file_path, 'r') as file:
             X_image = file['image'][...]
             y_category = file['category'][...]
     if verbose:
-        print("images: ",X_image.shape,"category:",y_category.shape)
+        print("images: ", X_image.shape, "category:", y_category.shape)
     return X_image, y_category
-        
 
 
-def get_dataframe(from_directory, as_name, rewrite=False, match_terms=[],exclude_terms=[] ):
+def get_dataframe(from_directory, as_name, rewrite=False, match_terms=[], exclude_terms=[]):
     file_path = join(from_directory, as_name)
     if not isfile(file_path) or rewrite:
         data = pd.DataFrame(columns=['filename', 'label'])
         # fetch if dataframe already exists
-        files = beda.get_list_dir(from_directory,with_extension='.npy',match_terms=match_terms,exclude_terms=exclude_terms)
-        for cat,file in files.items():
+        files = beda.get_list_dir(from_directory, with_extension='.npy',
+                                  match_terms=match_terms, exclude_terms=exclude_terms)
+        for cat, file in files.items():
             temp = pd.DataFrame()
             directory_name = join(from_directory, cat)
             temp['filename'] = sorted(listdir(directory_name))
@@ -235,9 +229,9 @@ def plot_images(images, images_categories, verbose=False):
         print(f"Prepare plots of {nb_images} images")
         print(f"Creating a grid {nrows}x{ncols}")
     for i in range(nb_images):
-        if isinstance(images_categories,str):
+        if isinstance(images_categories, str):
             title = images_categories
-        elif isinstance(images_categories,list):
+        elif isinstance(images_categories, list):
             title = images_categories[i]
         else:
             title = i
@@ -251,19 +245,20 @@ def plot_images(images, images_categories, verbose=False):
 
 
 def train_val_test_split(images, category, train_size=.75, val_size=.15, test_size=.1, verbose=False):
-    if isinstance(category,str):
+    if isinstance(category, str):
         y = np.full(images.shape[0], fill_value=category)
     else:
         y = category
 
     X_train, X_val, X_test, y_train, y_val, y_test = beda.train_val_test_split(
-        images, y, 
-        test_size=test_size, 
-        train_size=train_size, 
-        val_size=val_size, 
+        images, y,
+        test_size=test_size,
+        train_size=train_size,
+        val_size=val_size,
         random_state=1,
         show=verbose)
     return X_train, X_val, X_test, y_train, y_val, y_test
+
 
 def get_outlier_mask(images, verbose=False):
     """Create a mask to mark outlier
@@ -273,24 +268,26 @@ def get_outlier_mask(images, verbose=False):
 
     Returns:
         ndarray: array of bool
-    """    
+    """
     if verbose:
         print(f"Training on {images.shape[0]} images")
     # Isolation Forest to remove outlier
-    model = IsolationForest(contamination='auto', n_estimators=100,random_state=0,n_jobs=-1)
-    
+    model = IsolationForest(contamination='auto',
+                            n_estimators=100, random_state=0, n_jobs=-1)
+
     model.fit(images)
     if verbose:
         print("Predicting outliers")
     pred = model.predict(images)
     # crée un masque qui marque à True les anomalies
-    mask = (pred==-1)
+    mask = (pred == -1)
     # On applique le masque
     #outliers= images[mask]
-    #outliers.shape # ou mask.sum() pour connaître le nombre d'anomalies trouvées
+    # outliers.shape # ou mask.sum() pour connaître le nombre d'anomalies trouvées
     if verbose:
         print(f"Mask done, outliers found: {mask.sum()}")
     return mask
+
 
 def remove_outlier(images, verbose=False):
     mask = get_outlier_mask(images, verbose=verbose)
@@ -298,6 +295,7 @@ def remove_outlier(images, verbose=False):
     if verbose:
         print(f'Nb images after removing outliers: {filtered_images.shape[0]}')
     return filtered_images
+
 
 if __name__ == "__main__":
     pass
